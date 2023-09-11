c29c8a9e39090d01190c83e2728150154a636437
whalesync-ryder <93688278+whalesync-ryder@users.noreply.github.com>
2023-09-11 11:58:04 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Demonstration of a memory leak in prisma
+
+## Usage
+
+- `yarn install`
+- `yarn run start:dev`
+
+## Behavior
+
+This loops forever doing a bunch of prisma ops.
+Every 30 seconds, memory stats are logged.
+
+See `doOnePrimsaLoop()` for the operations performed.
+It's a mix of:
+
+- `create()` with nested `createMany()`
+- `$queryRaw()` to update some of the created records
+- `$transaction()` to delete what was just created
+
+## Leak
+
+The RSS increases by 0.8 Mb/min when left running.
+Here's a 27 hour run:
+https://docs.google.com/spreadsheets/d/1TPP-h-zk86fHhLOShuDtfZVF999_oqGrb05BSf0zPdA/edit#gid=34165034
---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Post, Prisma, PrismaClient } from '@prisma/client';
+import { MemoryLogger } from './memory-logger';
+
+const CONCURRENCY = 300;
+const POSTS_PER_USER = 10;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3007);
+}
+
+async function doPrismaWorkForever(): Promise<void> {
+  const prisma = new PrismaClient();
+  const memoryLogger = new MemoryLogger();
+
+  // Do this many concurrent creates & deletes.
+  while (true) {
+    await Promise.all(
+      Array.from({ length: CONCURRENCY }, () => doOnePrimsaLoop(prisma)),
+    );
+    memoryLogger.maybeLog();
+  }
+}
+
+async function doOnePrimsaLoop(prisma: PrismaClient): Promise<void> {
+  try {
+    // Create one user with a bunch of posts.
+    const createdUser = await prisma.user.create({
+      data: {
+        name: `Sample`,
+        email: `sample@email.com`,
+        posts: {
+          createMany: {
+            data: Array.from({ length: POSTS_PER_USER }, (_, i) => ({
+              title: `Sample Post ${i}`,
+              content: `content ${i}`,
+              published: false,
+            })),
+          },
+        },
+      },
+      include: { posts: true },
+    });
+
+    // Do an update on all the posts with queryRaw.
+    const postIds = createdUser.posts.map((p) => p.id);
+    await prisma.$queryRaw<Post>`update "Post" set "published" = TRUE where id = ANY(ARRAY[${Prisma.join(
+      postIds,
+    )}]) returning *`;
+
+    // Delete user and posts in one transaction.
+    await prisma.$transaction([
+      prisma.post.deleteMany({ where: { authorId: createdUser.id } }),
+      prisma.user.delete({ where: { id: createdUser.id } }),
+    ]);
+  } catch (e) {}
+}
+
+bootstrap();
+doPrismaWorkForever();
